c7db949fde7911c57e13fd53e46500ac2ab28b55
AowynB <aowynbrook@gmail.com>
2025-10-10 23:51:48 +0100

    Updated program to be able to accept a graph of size n link by link and generate a matrix for it.

--- Graph-Matrix_Converter/main.cpp
+++ Graph-Matrix_Converter/main.cpp
@@ -5,18 +5,19 @@
 #include <string>
 #include <vector>
 
+/* represents a single node in a one way graph, with pointers to connected noded */
 struct node{
-    char name;
-    std::vector<node*> neighbors;
+    char name; //node identifier
+    std::vector<node*> neighbors; //connected nodes
 };
 
-const int a = 'a';
+const int a = 'a'; //the ascii value of lowercase a, used for displaying graph nodes as alphabetical characters
 
+/* Declare available functions */
 static std::string matrix_to_graph(std::vector<std::vector<bool>>& matrix);
 static std::vector<std::vector<bool>> graph_to_matrix(std::vector<node*>& graph);
 
 int main(int argc, const char * argv[]) {
-    std::cout << a << std::endl;
     if(argc < 2){
         std::cerr << "ERROR: No Arguments Provided\n";
         return 1;
@@ -40,9 +41,29 @@ int main(int argc, const char * argv[]) {
     else if(argv[1][0] == 'g'){
         std::cin >> n;
         std::vector<node*> graph(n);
-
-        while(getline(std::cin, line)){
+        for(int i = 0; i < n; i++){
+            graph[i] = new node(char(a + i), std::vector<node*>());
         }
+
+        while(1){
+            std::cin >> line;
+            if(line == "END"){
+                break;
+            }
+
+            graph[line[0] - a]->neighbors.push_back(graph[line[3] - a]);
+        }
+
+        std::vector<std::vector<bool>> matrix = graph_to_matrix(graph);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                std::cout << matrix[i][j];
+            }
+            std::cout << std::endl;
+        }
+
+        for(auto node : graph){ delete node; }
+        return 0;
     }
 }
 
